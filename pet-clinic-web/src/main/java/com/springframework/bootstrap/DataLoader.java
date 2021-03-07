9f530f953c72dc82317f2b53096e55118698dec0
Mizkha <35262438+Mizkha@users.noreply.github.com>
2021-03-07 09:34:51 +0300

    adding spring config. Closes #22.

--- pet-clinic-web/src/main/java/com/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.springframework.petclinic.model.Owner;
 import com.springframework.petclinic.model.Vet;
 import com.springframework.petclinic.services.OwnerService;
 import com.springframework.petclinic.services.VetService;
-import com.springframework.petclinic.services.map.OwnerServiceMap;
-import com.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +13,14 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader(){
+    public DataLoader(OwnerService ownerService, VetService vetService){
 
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+//        ownerService = new OwnerServiceMap();
+//        vetService = new VetServiceMap();
 
 
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
